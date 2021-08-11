94f457b62b884650e5ed01da9cd491afb6cf52a7
Katie Pustolski <pustolskik@gmail.com>
2021-08-10 21:35:26 -0700

    Removed notes.txt. Created notes.json. Added code to create a new note in notes.json.

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -1,70 +1,54 @@
-// const fs = require(`fs`); // load in file system module
-// var path = "notes.txt";
-
-// fs.writeFileSync(path, 'This file was created by Node.js.');
-
-// //Append a message to notes.txt
-// fs.appendFileSync(path, "\nHello there!");
-// ---
-
-// Load file in from the same directory
-// const add = require("./utils.js");
-
-// const sum = add(4, -2);
-
-// console.log(sum);
-// ---
 // Load file in from the same directory
 const getNotes = require("./notes.js");
 const yargs = require("yargs");
 const chalk = require("chalk");
+const notes = require("./notes.js");
 
 yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    builder: {
-      // options for this command
-      title: {
-        // customize how this option works
-        describe: "Note title",
-        demandOption:true, // make --title option required
-        type: "string"
-      },
-      body: {
-        describe: "Add the body of the note",
-        demandOption: true,
-        type: "string"
-      }
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    // options for this command
+    title: {
+      // customize how this option works
+      describe: "Note title",
+      demandOption: true, // make --title option required
+      type: "string",
     },
-    handler: function(argv){
-      // argv holds the arguments from the options
-      console.log("Title: " + argv.title);
-      console.log("Body: " + argv.body);
-    }
-  });
-
-yargs.command({
-    command: "remove",
-    describe: "Removing a note",
-    handler:function(){
-        console.log("Removing a note");
-    }
+    body: {
+      describe: "Add the body of the note",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    // argv holds the arguments from the options
+    notes.addNote(argv.title, argv.body);
+  },
 });
 
 yargs.command({
-    command: "list",
-    describe: "List your notes",
-    handler: function(){
-        console.log("Listing all notes");
-    }
+  command: "remove",
+  describe: "Removing a note",
+  handler: function () {
+    console.log("Removing a note");
+  },
 });
 
 yargs.command({
-    command: "read",
-    describe: "Read a note",
-    handler:function(){
-        console.log("Reading a note");
-    }
+  command: "list",
+  describe: "List your notes",
+  handler: function () {
+    console.log("Listing all notes");
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("Reading a note");
+  },
 });
 
 yargs.parse();
--- NotesApp/notes.js
+++ NotesApp/notes.js
@@ -1,5 +1,54 @@
-const getNotes = function () {
-    return "Your notes...";
-}
+const fs = require("fs");
 
-module.exports = getNotes;
+const getNotes = function () {
+  return "Your notes...";
+};
+
+const addNote = function (title, body) {
+  const notes = loadNotes();
+
+  // Stores duplicates in the duplicateNotes array
+  // based on whether the titles match
+  // this will run for each note in the notes.json file
+  const duplicateNotes = notes.filter((note) => {
+    return note.title === title;
+  });
+
+  // No duplicates
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log("New note added!");
+  } else {
+    console.log("New title taken!");
+  }
+};
+
+// Pass in the notes array
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    // We know that there is no data if the file is not found.
+    // So we'll start adding to an empty array and create the file later
+    // in the code
+    return [];
+  }
+};
+
+// Export an object with properties.
+// This will allow us to use these properties in other files
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+};
