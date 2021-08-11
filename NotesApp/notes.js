a24a9ea9f00f56c43a463dacf722ae4c56a6f0f0
Katie Pustolski <pustolskik@gmail.com>
2021-08-10 22:12:59 -0700

    Added color to the addNote console.logs

--- NotesApp/notes.js
+++ NotesApp/notes.js
@@ -22,9 +22,9 @@ const addNote = function (title, body) {
       body: body,
     });
     saveNotes(notes);
-    console.log("New note added!");
+    console.log(chalk.green.inverse("New note added!"));
   } else {
-    console.log("New title taken!");
+    console.log(chalk.red.inverse("New title taken!"));
   }
 };
 
