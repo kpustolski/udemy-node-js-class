32b68f0d4dbc341feee958849935bca5619cf21d
Katie Pustolski <pustolskik@gmail.com>
2021-09-01 22:05:34 -0700

    Added call to geocode and forecast.
    * Remember, I didn't sign up the use the APIs, so these don't currently work

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode');
 
 const app = express();
 
@@ -48,10 +50,23 @@ app.get('/weather', (req, res) => {
     });
   }
 
-  res.send({
-    forcast: "it is snowing",
-    location: "Philadelphia",
-    address: req.query.address
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if(error){
+      return res.send({ error });
+    }
+
+    forecast(latitude, logitude, (error, forecastData) => {
+      if(error){
+        return res.send({ error });
+      }
+
+      // Everything went well by this point.
+      res.send({
+        forecast: forecastData,
+        location: location,
+        address: req.query.address
+      });
+    });
   });
 });
 
