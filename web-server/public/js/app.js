ad43220add6277135de5efe60e2fead98091caaa
Katie Pustolski <pustolskik@gmail.com>
2021-11-27 16:06:02 -0800

    Data resulting from the location search is printed to the web page.
    * Added CSS code fro the input and button tags
    * Removed some comments

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -3,22 +3,23 @@ console.log("this is a client side js file");
 //Select element from the HTML document that we want to work with
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
-// The default behavior of forms is to refresh the page when we hit
-// submit. We'll use the fetch call to dynamically add the output
-// onto the page so we don't need to refresh the page a bunch (can cause a flash of input, confusing the user)
 weatherForm.addEventListener('submit', (event)=>{
     // Prevents the default behavior of refreshing the page.
     event.preventDefault();
-    
+    messageOne.textContent = "Loading...";
+    messageTwo.textContent = "";
+
     const location = search.value;
     fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
-                console.log(data.error);
+                messageOne.textContent = data.error;
             }else{
-                console.log(data.location);
-                console.log(data.forecast);
+                messageOne.textContent = "Location: " + data.location;
+                messageTwo.textContent = "Forecase: " + data.forecast;
             }
         });
     });
